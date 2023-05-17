123175b45a1d104fe84bdac562b63dab1e6212ec
Srinivasan Venkataramanan <svenka3@gmail.com>
2023-05-17 17:20:34 +0100

    Added more SVA rules, tests

---
+++ sv_tests/test_sv_naming_19_f.sv
@@ -0,0 +1,20 @@
+//----------------------------------------------------
+// SPDX-FileCopyrightText: Srinivasan Venkataramanan, 
+//                         AsFigo Technologies, UK
+// SPDX-License-Identifier: MIT
+//----------------------------------------------------
+
+
+module sva_m;
+  bit var1;
+  bit clk;
+
+  property p_good_name;
+    var1;
+  endproperty : p_good_name
+
+  // BAD label should have c_ as prefix for cover
+  wrong_label : cover property (@(posedge clk) p_good_name);
+
+endmodule : sva_m
+
