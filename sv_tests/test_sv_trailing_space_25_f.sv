2e2cb7217e59611d54598e2091e7a4a7444f0741
rpjayaraman <jrp.postbox@gmail.com>
2023-09-09 19:39:36 +0530

    Adding testcase for trailing space check

---
+++ sv_tests/test_sv_trailing_space_25_f.sv
@@ -0,0 +1,10 @@
+//----------------------------------------------------
+// SPDX-FileCopyrightText: Jayaraman R P,
+//                         Verifworks Pvt Ltd,India
+// SPDX-License-Identifier: MIT
+//----------------------------------------------------
+class trailing_whitespace;
+  bit line_wo_trailing_space;
+  bit line_with_trailing_space; 
+
+endclass: trailing_whitespace
