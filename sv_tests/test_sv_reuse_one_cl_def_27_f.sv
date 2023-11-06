cd6f406f7c6e3850cc544192865d2f46a3beb585
prajapati93 <mehularvindprajapati@gmail.com>
2023-11-06 12:40:04 +0000

    added test files

---
+++ sv_tests/test_sv_reuse_one_cl_def_27_f.sv
@@ -0,0 +1,14 @@
+//----------------------------------------------------
+//----------------------------------------------------
+// SPDX-FileCopyrightText: Srinivasan Venkataramanan, 
+//                         AsFigo Technologies, UK
+// SPDX-License-Identifier: MIT
+//----------------------------------------------------
+
+class trans_c;
+  
+endclass : trans_c
+
+class drv_c;
+
+endclass : drv_c
