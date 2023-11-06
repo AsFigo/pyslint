d5431005117fef435bc8d33f547d97cd9d5a6834
prajapati93 <mehularvindprajapati@gmail.com>
2023-11-06 18:57:43 +0000

    Added new rule check

---
+++ sv_tests/test_sv_reuse_one_mod_def_27_f.sv
@@ -0,0 +1,14 @@
+//----------------------------------------------------
+//----------------------------------------------------
+// SPDX-FileCopyrightText: Srinivasan Venkataramanan, 
+//                         AsFigo Technologies, UK
+// SPDX-License-Identifier: MIT
+//----------------------------------------------------
+
+module top_m;
+  
+endmodule : top_m
+
+module design_m;
+
+endmodule : design_m
